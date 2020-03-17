323a51b3a3bb85bec8e0ae06cba21966885f829d
ayanrafique <ayanthechosenone@gmail.com>
2020-03-16 22:55:32 -0700

    Changed footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -61,9 +61,7 @@ const Layout = ({ location, title, children }) => {
       <header>{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      Copyright All Rights Reserved © {new Date().getFullYear()}, 
       </footer>
     </div>
   )
