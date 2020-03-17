c2a46ce990f4c71a7895cef76481bb64612b4ddf
ayanrafique <ayanthechosenone@gmail.com>
2020-03-16 23:22:47 -0700

    added breaks in bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -57,8 +57,8 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author.name}</strong> {author.summary}
-        {` `}
+        Written by <strong>{author.name}</strong> <br>{author.summary}
+        {` `}<br>
         <a href={`https://twitter.com/${social.twitter}`}>
           Follow me on Twitter!
         </a>
