f6f97ba488fd81bd1882f0c57ab4e75ee9084575
ayanrafique <ayanthechosenone@gmail.com>
2020-03-18 19:42:50 -0700

    One less space

--- src/components/bio.js
+++ src/components/bio.js
@@ -59,7 +59,7 @@ const Bio = () => {
       <p>
         Written by <strong>{author.name}.</strong> <br />{author.summary}.
         {` `} <br />
-        Follow me on &nbsp;
+        Follow me on&nbsp;
         <a href={`https://twitter.com/${social.twitter}`}>
       Twitter
         </a>
